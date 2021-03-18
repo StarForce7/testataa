77f0a0309553041448f54bd9cb8268043a2f56f4
StarForce7 <jopedidopedi@gmail.com>
2021-03-18 14:16:12 +0200

    Update ScriptiWat.js

--- ScriptiWat.js
+++ ScriptiWat.js
@@ -62,7 +62,7 @@ area: { // defines rectangle of the detection/localization area
             decoder: {
                 readers : ["ean_reader","ean_8_reader"],
 debug: {
-                        drawScanline: true}
+                        drawScanline: false}
             },
             locate: false
         },
@@ -72,7 +72,26 @@ debug: {
 
     App.init();
 
-    
+    Quagga.onProcessed(function(result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+        if (result) {
+            if (result.boxes) {
+                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                result.boxes.filter(function (box) {
+                    return box !== result.box;
+                }).forEach(function (box) {
+                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+                });
+            }
+            if (result.box) {
+                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+            }
+            if (result.codeResult && result.codeResult.code) {
+                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+            }
+        }
+    });
 
     Quagga.onDetected(function(result) {
         var code = result.codeResult.code;
