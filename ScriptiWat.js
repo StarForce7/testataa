4c3b0bde1dffeaecd06b42b4f8fd50e4ccf492a1
StarForce7 <jopedidopedi@gmail.com>
2021-03-18 18:04:58 +0200

    Update ScriptiWat.js

--- ScriptiWat.js
+++ ScriptiWat.js
@@ -86,7 +86,7 @@ drawBoundingBox: false}
                 });
             }
             if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 0}, drawingCtx, {color: "#00F", lineWidth: 2});
             }
             if (result.codeResult && result.codeResult.code) {
                 Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
