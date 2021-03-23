49f5eb85c0677fad29be4dca30ca739bbf5db93e
StarForce7 <jopedidopedi@gmail.com>
2021-03-23 12:39:51 +0200

    Update ScriptiWat.js

--- ScriptiWat.js
+++ ScriptiWat.js
@@ -119,6 +119,7 @@ if (err/countDecodedCodes < 0.08) {
     });
 
 });
+var paalla = false;
 document.getElementById('valo').onclick = function(e){if(paalla){var track = Quagga.CameraAccess.getActiveTrack();
 track.applyConstraints({advanced: [{torch: false}]}); paalla = false;}var track = Quagga.CameraAccess.getActiveTrack();
 track.applyConstraints({advanced: [{torch: true}]});paalla = true;}
