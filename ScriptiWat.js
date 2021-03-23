2fdc113399b2b3ed3c71e5d70d25291dd35d5eb8
StarForce7 <jopedidopedi@gmail.com>
2021-03-23 17:28:47 +0200

    Update ScriptiWat.js

--- ScriptiWat.js
+++ ScriptiWat.js
@@ -1,126 +1,119 @@
 document.getElementById('button').onclick = function(e){
-$("#interactive").show();
-$(function() {
+	$("#interactive").show();
+	$(function() {
 
-    var resultCollector = Quagga.ResultCollector.create({
-        capture: true,
-        capacity: 20,
+		var resultCollector = Quagga.ResultCollector.create({
+			capture: true,
+			capacity: 20,
+		});
+    
+		var App = {
+			init: function() {
+				var self = this;
+				Quagga.init(this.state, function(err) {
+					if (err) {
+						return self.handleError(err);
+					}
+					App.checkCapabilities();
+					Quagga.start();
+				});
+			},
+			handleError: function(err) {
+				console.log(err);
+			},
+			checkCapabilities: function() {
+				var track = Quagga.CameraAccess.getActiveTrack();
+				var capabilities = {};
+				if (typeof track.getCapabilities === 'function') {
+					capabilities = track.getCapabilities();
+				}
+			},
+
+			initCameraSelection: function(){
+				var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();    
+			},
         
-    });
-    var App = {
-        init: function() {
-            var self = this;
-            Quagga.init(this.state, function(err) {
-                if (err) {
-                    return self.handleError(err);
-                }
-                App.checkCapabilities();
-                Quagga.start();
-            });
-        },
-        handleError: function(err) {
-            console.log(err);
-        },
-        checkCapabilities: function() {
-            var track = Quagga.CameraAccess.getActiveTrack();
-            var capabilities = {};
-            if (typeof track.getCapabilities === 'function') {
-                capabilities = track.getCapabilities();
-            }
-        },
-
-
-   
-
-
-
-        initCameraSelection: function(){
-            var streamLabel = Quagga.CameraAccess.getActiveStreamLabel();    
-        },
-        
-        
-        
-        state: {
-            inputStream: {
-                type : "LiveStream",
-area: { // defines rectangle of the detection/localization area
-    top: "40%",    // top offset
-    right: "0%",  // right offset
-    left: "0%",   // left offset
-    bottom: "40%"  // bottom offset
-  },
-                constraints: {
-                    width: {min: 640},
-                    height: {min: 480},
-                    facingMode: "environment",
-                    aspectRatio: {min: 1, max: 2}
-                }
-            },
-            locator: {
-                patchSize: "medium",
-                halfSample: true
-            },
+			state: {
+				inputStream: {
+					type: "LiveStream",
+					area: {
+						top: "40%",
+						right: "0%",
+						left: "0%",
+						bottom: "40%"
+					},
+					constraints: {
+						width: {min: 640},
+						height: {min: 480},
+						facingMode: "environment",
+						aspectRatio: {min: 1, max: 100}
+					}
+				},
+				locator: {
+					patchSize: "medium",
+					halfSample: true
+				},
 	
-            numOfWorkers: navigator.hardwareConcurrency,
-            frequency: 7,
+				numOfWorkers: navigator.hardwareConcurrency,
+				frequency: 7,
+				decoder: {
+					readers : ["ean_reader","ean_8_reader"],
+					debug: {
+						drawScanline: true,
+						drawBoundingBox: false
+					}
+				},
+				locate: false
+			},
+			lastResult : null
+		};
 
-            decoder: {
-                readers : ["ean_reader","ean_8_reader"],
-debug: {
-                        drawScanline: true,
-drawBoundingBox: false}
-            },
-            locate: false
-        },
-        lastResult : null
-	
-    };
+		App.init();
 
-    App.init();
+		Quagga.onProcessed(function(result) {
+			var drawingCtx = Quagga.canvas.ctx.overlay,
+			drawingCanvas = Quagga.canvas.dom.overlay;
+			if (result) {
+				if (result.boxes) {
+					drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));  
+				}
+				if (result.codeResult && result.codeResult.code) {
+					Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+				}
+			}
+		});
 
-    Quagga.onProcessed(function(result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay;
-        if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                
-            }
+		Quagga.onDetected(function(result) {
+			var code = result.codeResult.code;
+			if (App.lastResult !== code) {
+				App.lastResult = code;
             
-            if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-            }
-        }
-    });
-
-    Quagga.onDetected(function(result) {
-        var code = result.codeResult.code;
-        if (App.lastResult !== code) {
-            App.lastResult = code;
-            
-var countDecodedCodes=0, err=0;
-$.each(result.codeResult.decodedCodes, function(id,error){
-    if (error.error!=undefined) {
-        countDecodedCodes++;
-        err+=parseFloat(error.error);
-    }
-});
-if (err/countDecodedCodes < 0.08) {
-     $input = $("#haku");
-	    $input.val(result.codeResult.code);
-	if(String(result.codeResult.code).length == 13){
-            $("#interactive").hide();
-	    Quagga.stop();
-        }}
-}
-
-	   
-
-    });
-
-});
-var paalla = 0;
-document.getElementById('valo').onclick = function(e){if(paalla === 1){var track = Quagga.CameraAccess.getActiveTrack();
-track.applyConstraints({advanced: [{torch: false}]}); paalla = 0;}else{var track = Quagga.CameraAccess.getActiveTrack();
-track.applyConstraints({advanced: [{torch: true}]});paalla = 1;}}
+				var countDecodedCodes=0, err=0;
+				$.each(result.codeResult.decodedCodes, function(id,error){
+					if (error.error!=undefined) {
+						countDecodedCodes++;
+						err+=parseFloat(error.error);
+					}
+				});
+				if (err/countDecodedCodes < 0.08) {
+					$input = $("#haku");
+					$input.val(result.codeResult.code);
+					if(String(result.codeResult.code).length == 13){
+						$("#interactive").hide();
+						Quagga.stop();
+					}
+				}
+			}
+		});
+	});
+	var paalla = 0;
+	document.getElementById('valo').onclick = function(e){
+		if(paalla === 1){var track = Quagga.CameraAccess.getActiveTrack();
+			track.applyConstraints({advanced: [{torch: false}]}); paalla = 0;
+		}
+		else{
+			var track = Quagga.CameraAccess.getActiveTrack();
+			track.applyConstraints({advanced: [{torch: true}]});paalla = 1;
+		}
+	}
 }
