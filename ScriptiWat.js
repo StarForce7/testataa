8c4351fb5a4dd3fdf4165f4d7a3ff6d303e2fd05
StarForce7 <jopedidopedi@gmail.com>
2021-04-08 08:35:28 +0300

    Update ScriptiWat.js

--- ScriptiWat.js
+++ ScriptiWat.js
@@ -100,8 +100,8 @@ document.getElementById('button').onclick = function(e){
 				//if (err / countDecodedCodes < 0.08) {
 					$input = $("#haku");
 					$input.val(result.codeResult.code);
-					if(String(result.codeResult.code).length >= 13 String(result.codeResult.code).includes("13606")){
-						if(String(result.codeResult.code).length === 14 &&  ){
+					if(String(result.codeResult.code).length >= 13 ){
+						if(String(result.codeResult.code).length === 14 &&  String(result.codeResult.code).includes("13606")){
 					$("#interactive").hide();
 					Quagga.stop();
 					}
